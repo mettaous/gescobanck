4e321d97e933b103628fafc63f566eca80413d30
mettaous <ettaous.mounir1995@gmail.com>
2025-03-14 23:42:31 +0100

    initial commit for new artifact

---
+++ src/main/java/com/sis/gescobank/GesCoBankApplication.java
@@ -0,0 +1,13 @@
+package com.sis.gescobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesCoBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesCoBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sis/gescobank/GesCoBankApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sis.gescobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesCoBankApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
